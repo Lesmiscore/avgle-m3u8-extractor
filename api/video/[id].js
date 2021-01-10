e9bddf1e40b6f1b279db0f8df38e17cc0d50a1bb
nao20010128nao <nao20010128@gmail.com>
2021-01-10 04:23:00 +0000

    それっぽいの

--- api/video/[id].js
+++ api/video/[id].js
@@ -1,5 +1,7 @@
 const chromium = require("chrome-aws-lambda");
 
+const wait = (ms) => new Promise((r) => setTimeout(r, ms));
+
 module.exports = async (req, res) => {
   const { id } = req.params;
   const { puppeteer } = chromium;
@@ -10,25 +12,50 @@ module.exports = async (req, res) => {
   });
   const page = await browser.newPage();
   try {
-    await page.setRequestInterception(true);
-    page.on("request", (request) => {
-      if (request.url() == "https://avgle.com/templates/frontend/videojs-contrib-hls.js") {
-        request.continue({
-          url: "PLAHEHOLDER",
+    await new Promise((resolve) => {
+      (async () => {
+        await page.exposeFunction("reportResponseText", (response) => {
+          res.writeHead(200, { "Content-Type": "application/x-mpegurl" });
+          res.end(response);
+          resolve();
         });
-      } else if (request.url().endsWith("?adblock_detected")) {
-        // easy adblocking
-        request.abort();
-      } else if (request.url().includes("jads.co")) {
-        // easy adblocking
-        request.abort();
-      } else {
-        request.continue();
-      }
+        await page.setRequestInterception(true);
+        page.on("request", (request) => {
+          if (request.url() == "https://avgle.com/templates/frontend/videojs-contrib-hls.js") {
+            request.continue({
+              // replace with patched code
+              url: "https://rawcdn.githack.com/nao20010128nao/avgle-m3u8-extractor/2d5ca7ed24db6bd2911ed5f2a1b72c08790c144e/videojs-contrib-hls.js",
+            });
+          } else if (request.url().endsWith("?adblock_detected")) {
+            // easy adblocking
+            request.abort();
+          } else if (request.url().includes("jads.co")) {
+            // easy adblocking
+            request.abort();
+          } else {
+            request.continue();
+          }
+        });
+        await page.goto(`https://avgle.com/video/${id}/`);
+        const [button] = await page.$x("//span[@id='player_3x2_close']");
+        if (button) {
+          await button.click();
+        }
+        await page.evaluate((_) => {
+          /* eslint no-undef: 0 */
+          closeAd({ screenX: 1, originalEvent: { isTrusted: true } });
+        });
+        await wait(100);
+        await page.evaluate((_) => {
+          /* eslint no-undef: 0 */
+          for (const p of Object.values(videojs.getPlayers())) {
+            p.play();
+          }
+        });
+      })();
     });
-    await page.goto(`https://avgle.com/video/${id}/`);
-    res.send(`Hello ${id}!`);
   } finally {
     await page.close();
+    await browser.close();
   }
 };
